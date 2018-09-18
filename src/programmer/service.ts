e5b8d773adcf6689a0191a8ef9893da65f9b5f98
Tero Tolonen <tero@sumpli.com>
2018-09-18 19:34:12 +0300

    cleaned code a bit

--- src/programmer/service.ts
+++ src/programmer/service.ts
@@ -9,48 +9,6 @@ const getTypePath = utils.getTypePath
 const getSwaggerType = utils.getSwaggerType
 const getMethodDoc = utils.getMethodDoc
 
-export interface EndpointParams {
-  httpMethod: string
-  basePath: string
-  apiPath: string
- 
-  pathParams: ParameterDeclaration[]
-  queryParams: ParameterDeclaration[]
-  bodyParams: ParameterDeclaration[]
-}
-
-/*
-  wr.out(`// Service endpoint for ${methodName}`, true);
-  wr.out(`app.${httpMethod}('${basePath}${apiPath}', async function( req, res ) {` , true)
-  wr.indent(1)
-
-  wr.out('try {', true)
-  wr.indent(1)
-    const pathArgs = pathParams.map( param => 'req.params.'+ param.getName() );
-    const queryArgs = queryParams.map( param => {
-      const pname = 'req.query.'+ param.getName()
-      if(getTypeName( param.getType() ) === 'boolean') {
-        return `typeof(${pname}) === 'undefined' ? ${pname} : ${pname} === 'true'`
-      }
-      return 'req.query.'+ param.getName() 
-    } );
-    const postArgs = bodyParams.length > 0 ? ['req.body'] : []
-    const paramList = [...pathArgs ,...queryArgs, ...postArgs].join(', ')
-    // name of the server
-    const servername = methodInfo.tags['using'] || 'server'; 
-    wr.out(`res.json( await ${servername}.${methodName}(${paramList}) );`, true)
-  wr.indent(-1)
-  wr.out('} catch(e) {', true)
-    wr.indent(1)
-    wr.out('res.status(400);', true)
-    wr.out(`res.json( e.message );`, true)
-    wr.indent(-1)
-  wr.out('}', true)
-  wr.indent(-1)
-  wr.out(`})`, true)
-*/
-
-
 export const initSwagger = (wr:R.CodeWriter, service:any) : R.CodeWriter => {
   const base = {  
     "swagger": "2.0",
@@ -116,12 +74,6 @@ export const WriteEndpoint = (wr:R.CodeWriter, project:Project, clName:ClassDecl
     bodyParams.push( methodParams[i] )
   }  
 
-  // the old way of creating params
-  /*
-  const getParams = method.getParameters().filter( param => isSimpleType(param.getType()) )
-  const postParams = method.getParameters().filter( param => !isSimpleType(param.getType()) )
-  const is_post = method.getParameters().filter( project => !isSimpleType(project.getType()) ).length > 0
-  */
   const is_post = bodyParams.length > 0 
   let httpMethod = methodInfo.tags.method || ( is_post ? 'post' : 'get' );
 
